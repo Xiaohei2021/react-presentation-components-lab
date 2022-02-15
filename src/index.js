cbaabdaf2c52539cf708a5847d4abcad0c349ce5
Leon <hualiang.tian@gmail.com>
2022-02-14 20:35:59 -0500

    commit tree 561abf041f46c902712a8ad4efd47adb8e0b4b3c

--- src/index.js
+++ src/index.js
@@ -5,9 +5,14 @@ import SimpleComponent from './components/SimpleComponent';
 import SimplerComponent from './components/SimplerComponent';
 
 ReactDOM.render(
+
+  onClick= () =>{
+    <h1>You clicked me</h1>
+  },
+
   <div>
     <SimpleComponent />
-    <SimplerComponent handleClick={undefined} />
+    <SimplerComponent handleClick={this.onClick} />
   </div>,
   document.getElementById('root')
 );
